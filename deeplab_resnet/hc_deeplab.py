205a23d145e5818bf7b74e50d13d5703cf908342
Ted Chao <tedchao850114@gmail.com>
2022-04-03 17:47:24 -0400

    tf v2 compatibility

--- deeplab_resnet/hc_deeplab.py
+++ deeplab_resnet/hc_deeplab.py
@@ -12,7 +12,7 @@ import time
 import tensorflow as tf
 import numpy as np
 
-from tensorflow.python.keras._impl.keras.initializers import he_normal
+# from tensorflow.python.keras._impl.keras.initializers import he_normal
 from tensorflow.python import debug as tf_debug
 
 from .base import Model
@@ -36,18 +36,18 @@ Helper functions
 '''
 
 def lowrank_linear(input_, dim_bottle, dim_out, name="lowrank_linear"):
-	with tf.variable_scope(name):
-		weights1 = tf.get_variable("fc_weights1", [input_.get_shape()[-1], dim_bottle], initializer=he_normal())
-		weights2 = tf.get_variable("fc_weights2", [dim_bottle, dim_out], initializer=he_normal())
-		biases = tf.get_variable("biases", [dim_out], initializer=tf.constant_initializer(0.01))
+	with tf.compat.v1.variable_scope(name):
+		weights1 = tf.compat.v1.get_variable("fc_weights1", [input_.get_shape()[-1], dim_bottle], initializer=tf.keras.initializers.HeNormal())
+		weights2 = tf.compat.v1.get_variable("fc_weights2", [dim_bottle, dim_out], initializer=tf.keras.initializers.HeNormal())
+		biases = tf.compat.v1.get_variable("biases", [dim_out], initializer=tf.compat.v1.constant_initializer(0.01))
 
 		activation = tf.add(tf.matmul(tf.matmul(input_, weights1), weights2), biases)
 	return activation
 	
 def linear(input_, dim_out, name="linear"):
-	with tf.variable_scope(name):
-		weights1 = tf.get_variable("fc_weights1", [input_.get_shape()[-1], dim_out], initializer=he_normal())
-		biases = tf.get_variable("fc_biases", [dim_out], initializer=tf.constant_initializer(0.01))
+	with tf.compat.v1.variable_scope(name):
+		weights1 = tf.compat.v1.get_variable("fc_weights1", [input_.get_shape()[-1], dim_out], initializer=tf.keras.initializers.HeNormal())
+		biases = tf.compat.v1.get_variable("fc_biases", [dim_out], initializer=tf.compat.v1.constant_initializer(0.01))
 
 		activation = tf.add(tf.matmul(input_, weights1), biases)
 	return activation
@@ -56,14 +56,14 @@ def linear(input_, dim_out, name="linear"):
 def conv2d(input_, output_dim, 
 		   k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
 		   name="conv2d"):
-	with tf.variable_scope(name):
-		w = tf.get_variable('weights', [k_h, k_w, input_.get_shape()[-1], output_dim],
-				  initializer=tf.truncated_normal_initializer(stddev=stddev))
-		conv = tf.nn.conv2d(input_, w, strides=[1, d_h, d_w, 1], padding='SAME')
+	with tf.compat.v1.variable_scope(name):
+		w = tf.compat.v1.get_variable('weights', [k_h, k_w, input_.get_shape()[-1], output_dim],
+				  initializer=tf.compat.v1.truncated_normal_initializer(stddev=stddev))
+		conv = tf.nn.conv2d(input=input_, filters=w, strides=[1, d_h, d_w, 1], padding='SAME')
 
-		biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
+		biases = tf.compat.v1.get_variable('biases', [output_dim], initializer=tf.compat.v1.constant_initializer(0.0))
 		# conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
-		conv = tf.reshape(tf.nn.bias_add(conv, biases), tf.shape(conv))
+		conv = tf.reshape(tf.nn.bias_add(conv, biases), tf.shape(input=conv))
 
 		return conv
 
@@ -91,32 +91,32 @@ class HyperColumn_Deeplabv2(Model):
 
 
 	def fn_map2visualization(self, full_embedding, sz2d):
-		randproj = tf.random_normal([FEAT_DIM,3])
+		randproj = tf.random.normal([FEAT_DIM,3])
 
 		visualized = tf.matmul(full_embedding, randproj)
 		tensorshape = tf.concat([tf.constant([-1]), sz2d, tf.constant([3])], 0)
 
 		visualized = tf.reshape(visualized, tensorshape)
-		maxval = tf.reduce_max(visualized)
-		minval = tf.reduce_min(visualized)
+		maxval = tf.reduce_max(input_tensor=visualized)
+		minval = tf.reduce_min(input_tensor=visualized)
 		visimg = tf.truediv(visualized - minval, maxval-minval)*255.0
 		return visimg
 
 # Deprecated
 	def lossfunction(self, tweightmat, tindicator, tembeddings):
 
-		with tf.variable_scope('loss_computation') as scope:
+		with tf.compat.v1.variable_scope('loss_computation') as scope:
 			# tembeddings: #pts x 64
-			sqrvals = tf.reduce_sum(tf.square(tembeddings), 1, keep_dims=True)
+			sqrvals = tf.reduce_sum(input_tensor=tf.square(tembeddings), axis=1, keepdims=True)
 			# sqrvals: #pts x 1
-			sqrvalsmat = tf.tile(sqrvals, [1, tf.shape(sqrvals)[0]])
-			sqrvalsmat2 = tf.add(sqrvalsmat,tf.transpose(sqrvalsmat))
-			distmat =  tf.add(sqrvalsmat2, tf.scalar_mul(-2.0, tf.matmul(tembeddings,  tf.transpose(tembeddings))))/64.0
+			sqrvalsmat = tf.tile(sqrvals, [1, tf.shape(input=sqrvals)[0]])
+			sqrvalsmat2 = tf.add(sqrvalsmat,tf.transpose(a=sqrvalsmat))
+			distmat =  tf.add(sqrvalsmat2, tf.scalar_mul(-2.0, tf.matmul(tembeddings,  tf.transpose(a=tembeddings))))/64.0
 
-			sigmamat = tf.scalar_mul(2.0, tf.reciprocal(1.0+tf.exp(distmat)))
-			posnegmapping = tf.log(tf.add(tf.scalar_mul(0.5, 1.0-tindicator), tf.multiply(tindicator, sigmamat)))
+			sigmamat = tf.scalar_mul(2.0, tf.math.reciprocal(1.0+tf.exp(distmat)))
+			posnegmapping = tf.math.log(tf.add(tf.scalar_mul(0.5, 1.0-tindicator), tf.multiply(tindicator, sigmamat)))
 			wcrossentropy = tf.multiply(tf.negative(tindicator+2.0), posnegmapping)
-			lossval = tf.reduce_mean(wcrossentropy)
+			lossval = tf.reduce_mean(input_tensor=wcrossentropy)
 		return lossval
 
 	def build_model(self):
@@ -126,32 +126,32 @@ class HyperColumn_Deeplabv2(Model):
 
 		# TF part: Input feeding
 		self.netcontainer = dict()
-		tinput_img = tf.placeholder(tf.float32,shape=(None,None,3),name='feed_img')
+		tinput_img = tf.compat.v1.placeholder(tf.float32,shape=(None,None,3),name='feed_img')
 		self.netcontainer['input_img'] = tinput_img
 
 		sz_lossmat = (NSAMPLEPTS*NINST,NSAMPLEPTS*NINST)
-		tlossweight = tf.placeholder(tf.float32, shape=sz_lossmat, name='const_weight')
+		tlossweight = tf.compat.v1.placeholder(tf.float32, shape=sz_lossmat, name='const_weight')
 		tlossindicator = tf.constant(npindicator, dtype=tf.float32, name='const_indicator')
-		tsample_points = tf.placeholder(tf.float32, shape=(NSAMPLEPTS*NINST, 2),name='feed_x2d')
+		tsample_points = tf.compat.v1.placeholder(tf.float32, shape=(NSAMPLEPTS*NINST, 2),name='feed_x2d')
 		self.netcontainer['input_weightmat'] = tlossweight
 		self.netcontainer['input_samplepts'] = tsample_points
 
-		input_img = tf.expand_dims(tinput_img, dim=0)
+		input_img = tf.expand_dims(tinput_img, axis=0)
 		
 		# Create network.
-		with tf.variable_scope('', reuse=False):
+		with tf.compat.v1.variable_scope('', reuse=False):
 			net = DeepLabResNetModel({'data': input_img}, is_training=self.args.is_training, num_classes=self.args.num_classes)
 		
 		self.netcontainer['net'] = net
 
-		t_imsz = tf.shape(input_img)[1:3]
+		t_imsz = tf.shape(input=input_img)[1:3]
 
-		with tf.variable_scope('hypercolumn_layers') as scope:
+		with tf.compat.v1.variable_scope('hypercolumn_layers') as scope:
 			raw_color = conv2d(input_img/128.0, 4, k_h=1, k_w=1, d_h=1, d_w=1, name="hc_cv_0")
-			raw_1 = tf.image.resize_bilinear(conv2d(net.layers['pool1'], 124, k_h=1, k_w=1, d_h=1, d_w=1, name="hc_cv_1"), t_imsz)
-			raw_3 = tf.image.resize_bilinear(conv2d(net.layers['res3b3_relu'], 128, k_h=1, k_w=1, d_h=1, d_w=1, name="hc_cv_3"), t_imsz)
-			raw_4 = tf.image.resize_bilinear(conv2d(net.layers['res4b22_relu'], 256, k_h=3, k_w=3, d_h=1, d_w=1, name="hc_cv_4"), t_imsz)
-			raw_5 = tf.image.resize_bilinear(conv2d(net.layers['res5c'], 512, k_h=3, k_w=3, d_h=1, d_w=1, name="hc_cv_5"), t_imsz)
+			raw_1 = tf.image.resize(conv2d(net.layers['pool1'], 124, k_h=1, k_w=1, d_h=1, d_w=1, name="hc_cv_1"), t_imsz, method=tf.image.ResizeMethod.BILINEAR)
+			raw_3 = tf.image.resize(conv2d(net.layers['res3b3_relu'], 128, k_h=1, k_w=1, d_h=1, d_w=1, name="hc_cv_3"), t_imsz, method=tf.image.ResizeMethod.BILINEAR)
+			raw_4 = tf.image.resize(conv2d(net.layers['res4b22_relu'], 256, k_h=3, k_w=3, d_h=1, d_w=1, name="hc_cv_4"), t_imsz, method=tf.image.ResizeMethod.BILINEAR)
+			raw_5 = tf.image.resize(conv2d(net.layers['res5c'], 512, k_h=3, k_w=3, d_h=1, d_w=1, name="hc_cv_5"), t_imsz, method=tf.image.ResizeMethod.BILINEAR)
 
 			raw_output = tf.nn.relu(tf.concat([raw_color, raw_1, raw_3, raw_4, raw_5], 3))
 			
@@ -159,21 +159,21 @@ class HyperColumn_Deeplabv2(Model):
 			full_activation = tf.reshape(raw_output, [-1, nfeatdim])
 			
 			# FC layes
-		with tf.variable_scope('fc1_matting') as scope:
+		with tf.compat.v1.variable_scope('fc1_matting') as scope:
 			full_act1 = tf.nn.relu(lowrank_linear(full_activation, 256, 512, name="linear"))
 			
-		with tf.variable_scope('fc2_matting') as scope:
+		with tf.compat.v1.variable_scope('fc2_matting') as scope:
 			full_act2 = tf.nn.relu(lowrank_linear(full_act1, 256, 512, name="linear"))
 
-		with tf.variable_scope('fc3_matting') as scope:
+		with tf.compat.v1.variable_scope('fc3_matting') as scope:
 			full_input3 = tf.concat([full_act1, full_act2], -1) 	# similar to DenseNet
 			full_embedding = linear(full_input3, FEAT_DIM)
 			
 
 		# embeddings: #pts x FEAT_DIM
-		visimg = self.fn_map2visualization(full_embedding, tf.shape(raw_output)[1:3])
+		visimg = self.fn_map2visualization(full_embedding, tf.shape(input=raw_output)[1:3])
 		
-		outshape = tf.concat([tf.shape(raw_output)[0:3], tf.constant([-1])], 0)
+		outshape = tf.concat([tf.shape(input=raw_output)[0:3], tf.constant([-1])], 0)
 		self.netcontainer['out_hypcol'] = tf.reshape(full_embedding, outshape)
 		self.netcontainer['out_visimg'] = visimg
 
@@ -185,9 +185,9 @@ class HyperColumn_Deeplabv2(Model):
 		args = self.args
 		# Which variables to load. Running means and variances are not trainable,
 		# thus all_variables() should be restored.
-		restore_var = [v for v in tf.global_variables()]
+		restore_var = [v for v in tf.compat.v1.global_variables()]
 
-		all_trainable = [v for v in tf.trainable_variables() if 'beta' not in v.name and 'gamma' not in v.name]
+		all_trainable = [v for v in tf.compat.v1.trainable_variables() if 'beta' not in v.name and 'gamma' not in v.name]
 		fc_trainable = [v for v in all_trainable if 'fc' in v.name]
 		conv_trainable = [v for v in all_trainable if 'fc' not in v.name] # lr * 1.0
 		fc_w_trainable = [v for v in fc_trainable if 'weights' in v.name] # lr * 10.0
@@ -197,12 +197,12 @@ class HyperColumn_Deeplabv2(Model):
 	 
 		# Define loss and optimisation parameters.
 		base_lr = tf.constant(args.learning_rate)
-		step_ph = tf.placeholder(dtype=tf.float32, shape=(), name='ph_step')
+		step_ph = tf.compat.v1.placeholder(dtype=tf.float32, shape=(), name='ph_step')
 		learning_rate = tf.scalar_mul(base_lr, tf.pow((1 - step_ph / args.num_steps), args.power))
 		
-		opt_conv = tf.train.MomentumOptimizer(learning_rate, args.momentum)
-		opt_fc_w = tf.train.MomentumOptimizer(learning_rate * 10.0, args.momentum)
-		opt_fc_b = tf.train.MomentumOptimizer(learning_rate * 20.0, args.momentum)
+		opt_conv = tf.compat.v1.train.MomentumOptimizer(learning_rate, args.momentum)
+		opt_fc_w = tf.compat.v1.train.MomentumOptimizer(learning_rate * 10.0, args.momentum)
+		opt_fc_b = tf.compat.v1.train.MomentumOptimizer(learning_rate * 20.0, args.momentum)
 
 		# Define a variable to accumulate gradients.
 		accum_grads = [tf.Variable(tf.zeros_like(v.initialized_value()),
@@ -212,7 +212,7 @@ class HyperColumn_Deeplabv2(Model):
 		zero_op = [v.assign(tf.zeros_like(v)) for v in accum_grads]
 
 		# Compute gradients.
-		grads = tf.gradients(self.loss, conv_trainable + fc_w_trainable + fc_b_trainable)
+		grads = tf.gradients(ys=self.loss, xs=conv_trainable + fc_w_trainable + fc_b_trainable)
 	 
 		# Accumulate and normalise the gradients.
 		accum_grads_op = [accum_grads[i].assign_add(tf.scalar_mul(1.0/np.float32(args.grad_update_every), grad)) for i, grad in
@@ -260,24 +260,24 @@ class HyperColumn_Deeplabv2(Model):
 		caller_sampler = [pt_sampler['out_batchx2d'], pt_sampler['out_weightmat']]
 
 		# Pixel-wise softmax loss.
-		l2_losses = [args.weight_decay * tf.nn.l2_loss(v) for v in tf.trainable_variables() if 'weights' in v.name]
+		l2_losses = [args.weight_decay * tf.nn.l2_loss(v) for v in tf.compat.v1.trainable_variables() if 'weights' in v.name]
 		self.loss = tf.add_n(l2_losses)
 
 		# Processed predictions: for visualisation.
-		pred = tf.cast(tf.image.resize_bilinear(self.netcontainer['out_visimg'], input_size), tf.uint8)
+		pred = tf.cast(tf.image.resize(self.netcontainer['out_visimg'], input_size, method=tf.image.ResizeMethod.BILINEAR), tf.uint8)
 
 		# Image summary.
-		images_summary = tf.py_func(inv_preprocess, [tf.expand_dims(self.netcontainer['input_img'], dim=0), args.save_num_images, IMG_MEAN], tf.uint8)
+		images_summary = tf.compat.v1.py_func(inv_preprocess, [tf.expand_dims(self.netcontainer['input_img'], axis=0), args.save_num_images, IMG_MEAN], tf.uint8)
 
-		total_summary = tf.summary.image('images', tf.concat(axis=2, values=[images_summary, pred]), 
+		total_summary = tf.compat.v1.summary.image('images', tf.concat(axis=2, values=[images_summary, pred]), 
 												 max_outputs=args.save_num_images) # Concatenate row-wise.
-		summary_writer = tf.summary.FileWriter(args.snapshot_dir, graph=tf.get_default_graph())
+		summary_writer = tf.compat.v1.summary.FileWriter(args.snapshot_dir, graph=tf.compat.v1.get_default_graph())
 		self.setup_optimizer()
 
 
 		self.step = self.train_container['step_ph']
 
-		tf.global_variables_initializer().run()
+		tf.compat.v1.global_variables_initializer().run()
 		self.load(self.checkpoint_dir)
 
 		start_time = time.time()
